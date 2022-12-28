b6967ff736ade2b3b65b67e4a37e9e58a6795ca2
turkishman41 <89349479+turkishman41@users.noreply.github.com>
2022-12-28 03:48:17 +0330

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,79 @@
+class Translation(object):
+
+    START_TEXT = """Esenlikler, Sayğılar {},
+
+Ben URL Yükleyiciyim!
+
+Bu botu kullanarak HTTP/HTTPS  bağlantısını 1.96 Boyutuna Kadar Telegram'a yükleyebilirsiniz!
+
+/help daha çok Bilgiler için ! 
+
+©️ @DTO_Bots
+
+"""
+
+    FORMAT_SELECTION = "<b>İstediyiniz Biçimi Seçiniz!\n</b>"
+
+    SET_CUSTOM_USERNAME_PASSWORD = """
+
+🎬 Premium videolari indirmek istiyorsanız ?!
+
+Aşağıdaki biçimde Gönderin:
+
+🔗 URL | dosya adı | kullanıcı adı | şifre"""
+
+    DOWNLOAD_START = "⤵️ İndirlir..."
+
+    UPLOAD_START = "⤴️Yüklenir..."
+
+    RCHD_TG_API_LIMIT = "{} saniye içinde indirildi⤵️.\nAlgılanan Dosya Boyutu: {}\n☹️Üzgünüm. Ancak Telegram API sınırlamaları nedeniyle 2 GB'tan büyük dosyaları yükleyemiyorum."
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Kullandığınız için teşekkürler @DTO_URL_UploderBot \n\n 🆑 Katıl : 🤖 @DTO_Bots "
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "⬇️{} saniye içinde indirildi.\n⬆️{} saniye içinde yüklendi.\n\n🤖@DTO_URL_UploderBot"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Özel video / dosya küçük resmi kaydedildi. Bu görsel videoda/dosyada kullanılacaktır."
+
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Özel küçük resim başarıyla Silindi."
+
+    CUSTOM_CAPTION_UL_FILE = "{}"
+
+    NO_VOID_FORMAT_FOUND = "HATA...\n<b>YouTubeDL</b> dedi: {}"
+
+    HELP_USER = """ 
+
+🎛 Yardım Menusu
+
+🔰 Botu Nasıl Kullana Bilirsiniz ? Bu adımları takip et!
+
+    
+
+1. URL'yi gönderin (ornek.domain/dosya.mp4 | Yeni DosyaAdı.mp4).
+
+2. Özel Küçük Resim Olarak Gönder (İsteseniz).
+
+3. Düğmeyi seçin.
+
+   SVideo - Dosyayı Ekran Görüntüleriyle birlikte video olarak vereck
+
+   DFile - Dosyayı (videoyu) Ekran Görüntüleri ile dosya olarak verir
+
+   Video - Dosyayı Ekran Görüntüleri olmadan video olarak verecek
+
+   Dosya - Dosyayı Ekran Görüntüsü Olmadan Verecek
+
+Bot yanıt vermediyse, 🤖 @DTO_Bots ile iletişime geçin"""
+
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Özel küçük resim oluşturmak için bir medya albümüne yanıt verin/özel küçük resim oluşturun"
+
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Medya Albümü yalnızca iki fotoğraf içermelidir. Lütfen medya albümünü yeniden gönderin ve ardından tekrar deneyin veya bir albümde yalnızca iki fotoğraf gönderin."
+
+Özel küçük resim desteği ile yeniden adlandırmak için dosyayı aldıktan sonra /rename komutunu kullanabilirsiniz.
+
+"""
+
+    CANCEL_STR = "❌ İşlem İptal Edildi"
+
+    ZIP_UPLOADED_STR = "{} dosya {} saniyede ⬆️yüklendi"
+
+    SLOW_URL_DECED = "Aman Tanrım, çok yavaş bir URL gibi görünüyor. Evimi mahvettiğin için bu dosyayı indirecek Zorunda değilim.\nTelegram'a yükleme yapabilmem için bana hızlı bir URL Gönder"
